2b82a7e86dd19242c3655317e363ba323f230007
thoma <thoma@DESKTOP-C0Q0P9K>
2016-09-22 08:46:24 +0200

    har lavet noget af en http server

---
+++ Teknik_mockExam_2016-09-21/Teknik_mockExam_2016-09-21/EchoService.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Sockets;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Teknik_mockExam_2016_09_21
+{
+    public class EchoService
+    {
+        private readonly TcpClient _connectionSocket;
+
+        /// <summary>
+        /// The path to the file, the path can be changed after need
+        /// </summary>
+        private static readonly string RootCatalog =
+            @"C:\Users\thoma\Source\Repos\datamatiker-semester-3\Teknik_mockExam_2016-09-21\Teknik_mockExam_2016-09-21\rootCatalog\";
+
+        public EchoService(TcpClient connectionSocket)
+        {
+            _connectionSocket = connectionSocket;
+        }
+
+        /// <summary>
+        /// uses the path from RootCatalog og splits it up and returns the path
+        /// it used in the method Message()
+        /// </summary>
+        /// <returns></returns>
+        private static string Root()
+        {
+            string dir = RootCatalog;
+            string[] parts = dir.Split('\\');
+
+            foreach (string part in parts)
+            {
+                Console.WriteLine(part);
+            }
+            return dir;
+        }
+
+        /// <summary>
+        /// This method open and closes streams.
+        /// its making a HTTP GET response to the browser and gives the browers the file which it opens and reads
+        /// afterward it closes the connection, and the brower get its response and shows the file
+        /// </summary>
+        internal void Message()
+        {
+            using (_connectionSocket)
+            {
+                try
+                {
+                    Stream cs = _connectionSocket.GetStream();
+
+                    StreamReader sr = new StreamReader(cs);
+                    StreamWriter sw = new StreamWriter(cs) {AutoFlush = true}; // enable automatic flushing
+
+                    string message = " ";
+
+                    while (message != null && message.ToLower() != "stop")
+                    {
+                        message = sr.ReadLine();
+
+                        Console.WriteLine(message);
+
+                        // Makes a request on the browser request
+                        // if the message contains something which is not null, and the message contains a GET and http 1.1 request, it does something
+                        if (message != null && (message.Contains("GET") && message.Contains("HTTP/1.1")))
+                        {
+                            try
+                            {
+                                // The file which it should open
+                                message = File.ReadAllText(Root() + @"testfil.txt");
+                            }
+                            catch (Exception e)
+                            {
+                                throw e;
+                            }
+                            sw.WriteLine(message.ToLower());
+                        }
+                        else
+                        {
+                            if (message != null) sw.WriteLine(message.ToLower());
+                        }
+
+                        // Closes the connection to the server
+                        cs.Close();
+                        _connectionSocket.Close();
+                    }
+                }
+                catch (Exception e)
+                {
+                    throw e;
+                }
+            }
+        }
+    }
+}
---
+++ Teknik_mockExam_2016-09-21/Teknik_mockExam_2016-09-21/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Teknik_mockExam_2016_09_21
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            // IP address which the server uses, 127.0.0.1 is localhost, can be changed after need
+            IPAddress ip = IPAddress.Parse("127.0.0.1");
+            try
+            {
+                // which IP and port the server should listen to
+                TcpListener myServer = new TcpListener(ip, 80);
+                //starter serveren
+                myServer.Start();
+
+                // runs a while loop
+                while (true)
+                {
+                    TcpClient connectionSocket = myServer.AcceptTcpClient();
+                    Console.WriteLine("Server started");
+                    // uses the class EchoServer, which reads and writes the message
+                    EchoService service = new EchoService(connectionSocket);
+                    //service.Message();
+                    service.Message();
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+        }
+    }
+}
